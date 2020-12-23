25e911a0b7d57f0cea8baa1dac370d9f38112900
John <jmacpherson@homex.com>
2020-12-23 10:31:10 -0500

    start building out ability to delegate on iOS

--- ios/Classes/SwiftOcarinaPlugin.swift
+++ ios/Classes/SwiftOcarinaPlugin.swift
@@ -88,9 +88,73 @@ class SinglePlayer: Player {
     }
 }
 
+class PlayerDelegate {
+    let loadDelegate: LoadDelegate
+    let playDelegate: PlayDelegate
+    let pauseDelegate: PauseDelegate
+    let resumeDelegate: ResumeDelegate
+    let stopDelegate: StopDelegate
+    let volumeDelegate: VolumeDelegate
+    
+    func load(_ id: Int, assetUrl: String, volume: Double, loop: Bool) {
+        let url: URL = URL(fileURLWithPath: assetUrl)
+        let fileForPlayback: AVAudioFile?
+        do {
+            fileForPlayback = try AVAudioFile(forReading: url)
+        } catch let error {
+            NSLog("Cannot play music. Error opening file \(url) for reading \(error).")
+            return
+        }
+        
+        guard let file = fileForPlayback else {
+            NSLog("Cannot play music. Cannot open file for reading \(url).")
+            return
+        }
+
+        loadDelegate(id, file, loop, volume)
+    }
+    
+    func play(_ id: Int) {
+        playDelegate(id)
+    }
+
+    func pause(_ id: Int) {
+        pauseDelegate(id)
+    }
+
+    func resume(_ id: Int) {
+        resumeDelegate(id)
+    }
+
+    func stop(_ id: Int) {
+        stopDelegate(id)
+    }
+
+    func volume(_ id: Int, volume: Double) {
+        volumeDelegate(id, volume)
+    }
+    
+    init(load: @escaping LoadDelegate, play: @escaping PlayDelegate, pause: @escaping PauseDelegate, resume: @escaping ResumeDelegate, stop: @escaping StopDelegate, volume: @escaping VolumeDelegate) {
+        loadDelegate = load
+        playDelegate = play
+        pauseDelegate = pause
+        resumeDelegate = resume
+        stopDelegate = stop
+        volumeDelegate = volume
+    }
+}
+
+public typealias LoadDelegate = (_ id: Int, _ file: AVAudioFile, _ loop: Bool, _ volume: Double) -> Void
+public typealias PlayDelegate = (_ id: Int) -> Void
+public typealias PauseDelegate = (_ id: Int) -> Void
+public typealias ResumeDelegate = (_ id: Int) -> Void
+public typealias StopDelegate = (_ id: Int) -> Void
+public typealias VolumeDelegate = (_ id: Int, _ volume: Double) -> Void
+
 public class SwiftOcarinaPlugin: NSObject, FlutterPlugin {
     static var players = [Int: Player]()
     static var id: Int = 0;
+    static var delegate: PlayerDelegate?
     var registrar: FlutterPluginRegistrar? = nil
     
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -99,131 +163,196 @@ public class SwiftOcarinaPlugin: NSObject, FlutterPlugin {
         registrar.addMethodCallDelegate(instance, channel: channel)
         instance.registrar = registrar
     }
+
+    public static func useDelegate(load: @escaping LoadDelegate, play: @escaping PlayDelegate, pause: @escaping PauseDelegate, resume: @escaping ResumeDelegate, stop: @escaping StopDelegate, volume: @escaping VolumeDelegate) {
+        delegate = PlayerDelegate(load: load, play: play, pause: pause, resume: resume, stop: stop, volume: volume)
+    }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if (call.method == "load") {
-            guard let args = call.arguments else {
-                return;
-            }
-            
-            if let myArgs = args as? [String: Any],
-                let url: String = myArgs["url"] as? String,
-                let volume: Double = myArgs["volume"] as? Double,
-                let isAsset: Bool = myArgs["isAsset"] as? Bool,
-                let loop: Bool = myArgs["loop"] as? Bool {
-                
-                var assetUrl: String
-                if (isAsset) {
-                    let key: String?
-                    if let package: String = myArgs["package"] as? String {
-                     key = registrar?.lookupKey(forAsset: url, fromPackage: package)
-                    } else {
-                     key = registrar?.lookupKey(forAsset: url)
-                    }
-
-                    if let url = Bundle.main.path(forResource: key, ofType: nil) {
-                        assetUrl = url
-                    } else {
-                        return result(FlutterError(code: "ASSET_URL_NOT_FOUND", message: "key: " + (key ?? ""), details: nil))
-                    }
-                } else {
-                    assetUrl = url
-                }
-                
-                let id = SwiftOcarinaPlugin.id
-                if (loop) {
-                    SwiftOcarinaPlugin.players[id] = LoopPlayer(url: assetUrl, volume: volume)
-                } else {
-                    SwiftOcarinaPlugin.players[id] = SinglePlayer(url: assetUrl, volume: volume)
-                }
-                
-                SwiftOcarinaPlugin.id = SwiftOcarinaPlugin.id + 1
-                
-                result(id)
-            }
+            load(call, result: result)
         } else if (call.method == "play") {
-            guard let args = call.arguments else {
-                return;
+            play(call, result: result)
+        } else if (call.method == "pause") {
+            pause(call, result: result)
+        } else if (call.method == "stop") {
+            stop(call, result: result)
+        } else if (call.method == "volume") {
+            volume(call, result: result)
+        } else if (call.method == "resume") {
+            resume(call, result: result)
+        } else if (call.method == "seek") {
+            seek(call, result: result)
+        } else if (call.method == "dispose") {
+            dispose(call, result: result)
+        }
+    }
+    
+    func load(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let url: String = myArgs["url"] as? String,
+            let volume: Double = myArgs["volume"] as? Double,
+            let isAsset: Bool = myArgs["isAsset"] as? Bool,
+            let loop: Bool = myArgs["loop"] as? Bool {
+            
+            var assetUrl: String
+            if (isAsset) {
+                let key: String?
+                if let package: String = myArgs["package"] as? String {
+                 key = registrar?.lookupKey(forAsset: url, fromPackage: package)
+                } else {
+                 key = registrar?.lookupKey(forAsset: url)
+                }
+
+                if let url = Bundle.main.path(forResource: key, ofType: nil) {
+                    assetUrl = url
+                } else {
+                    return result(FlutterError(code: "ASSET_URL_NOT_FOUND", message: "key: " + (key ?? ""), details: nil))
+                }
+            } else {
+                assetUrl = url
             }
             
-            if let myArgs = args as? [String: Any],
-                let playerId: Int = myArgs["playerId"] as? Int {
-                
+            let id = SwiftOcarinaPlugin.id
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                delegate.load(id, assetUrl: assetUrl, volume: volume, loop: loop)
+            } else if (loop) {
+                SwiftOcarinaPlugin.players[id] = LoopPlayer(url: assetUrl, volume: volume)
+            } else {
+                SwiftOcarinaPlugin.players[id] = SinglePlayer(url: assetUrl, volume: volume)
+            }
+            
+            SwiftOcarinaPlugin.id = SwiftOcarinaPlugin.id + 1
+            
+            result(id)
+        }
+    }
+    
+    func play(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int {
+            
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                delegate.play(playerId)
+            } else {
                 let player = SwiftOcarinaPlugin.players[playerId]
                 player?.play()
-                result(0)
             }
-        } else if (call.method == "pause") {
-                   guard let args = call.arguments else {
-                       return;
-                   }
-                   
-                   if let myArgs = args as? [String: Any],
-                       let playerId: Int = myArgs["playerId"] as? Int {
-                       
-                       let player = SwiftOcarinaPlugin.players[playerId]
-                       player?.pause()
-                       result(0)
-                   }
-        } else if (call.method == "stop") {
-            guard let args = call.arguments else {
-                return;
+            result(0)
+        }
+    }
+    
+    func pause(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int {
+         
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                delegate.pause(playerId)
+            } else {
+                let player = SwiftOcarinaPlugin.players[playerId]
+                player?.pause()
             }
-            
-            if let myArgs = args as? [String: Any],
-                let playerId: Int = myArgs["playerId"] as? Int {
-                
+            result(0)
+        }
+    }
+    
+    func stop(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int {
+
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                delegate.stop(playerId)
+            } else {
                 let player = SwiftOcarinaPlugin.players[playerId]
                 player?.stop()
-                result(0)
             }
+            result(0)
+        }
+    }
+    
+    func volume(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int,
+            let volume: Double = myArgs["volume"] as? Double {
             
-        } else if (call.method == "volume") {
-            guard let args = call.arguments else {
-                return;
-            }
-            
-            if let myArgs = args as? [String: Any],
-                let playerId: Int = myArgs["playerId"] as? Int,
-                let volume: Double = myArgs["volume"] as? Double {
-                
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                delegate.volume(playerId, volume: volume)
+            } else {
                 let player = SwiftOcarinaPlugin.players[playerId]
                 player?.volume(volume: volume)
-                result(0)
             }
-        } else if (call.method == "resume") {
-            guard let args = call.arguments else {
-                return;
-            }
+            result(0)
+        }
+    }
+    
+    func resume(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int {
             
-            if let myArgs = args as? [String: Any],
-                let playerId: Int = myArgs["playerId"] as? Int {
-                
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                delegate.resume(playerId)
+            } else {
                 let player = SwiftOcarinaPlugin.players[playerId]
                 player?.resume()
-                result(0)
             }
-        } else if (call.method == "seek") {
-            guard let args = call.arguments else {
-                return;
-            }
+            result(0)
+        }
+    }
+    
+    func seek(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int,
+            let positionInMillis: Int = myArgs["position"] as? Int {
             
-            if let myArgs = args as? [String: Any],
-                let playerId: Int = myArgs["playerId"] as? Int,
-                let positionInMillis: Int = myArgs["position"] as? Int {
-                
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                result(FlutterMethodNotImplemented)
+            } else {
                 let player = SwiftOcarinaPlugin.players[playerId]
                 player?.seek(position: positionInMillis)
                 result(0)
             }
-        } else if (call.method == "dispose") {
-            guard let args = call.arguments else {
-                return;
-            }
+        }
+    }
+    
+    func dispose(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments else {
+            return;
+        }
+        
+        if let myArgs = args as? [String: Any],
+            let playerId: Int = myArgs["playerId"] as? Int {
             
-            if let myArgs = args as? [String: Any],
-                let playerId: Int = myArgs["playerId"] as? Int {
-                
+            if let delegate = SwiftOcarinaPlugin.delegate {
+                result(FlutterMethodNotImplemented)
+            } else {
                 let player = SwiftOcarinaPlugin.players[playerId]
                 player?.stop()
                 SwiftOcarinaPlugin.players[playerId] = nil
